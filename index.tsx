62da14350bdffbafb8e94d184b25ca945b48f29a
Margot Fournier <margot.fournier@hymaia.com>
2025-12-30 09:55:37 +0100

    Fix tailwindcss error

--- index.tsx
+++ index.tsx
@@ -1,4 +1,6 @@
 
+import './styles.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './index.tsx',
+    './App.tsx',
+    './components/**/*.{ts,tsx}',
+    './constants.ts',
+    './types.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
